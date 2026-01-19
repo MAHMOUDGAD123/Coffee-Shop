6cd68286e5c896b8c9f3c44dc4a7aa8058b87cbe
Mahmoud_Gad <hooda.gad333@gmail.com>
2026-01-19 13:40:34 +0200

    update

--- app/components/layout/LayoutHeader.vue
+++ app/components/layout/LayoutHeader.vue
@@ -4,9 +4,6 @@
   const isSmallScreen = useMediaQuery('(width >= 640px)');
   const windowScroll = useWindowScroll({
     throttle: 50,
-    onScroll() {
-      createLogger().success(windowScroll.y.value);
-    },
   });
 
   // --- Computed ---
